fa3e4950d9dafe9aca6a75c5dc0aec3ff2af8b73
sindaZeng <1692488900@qq.com>
2020-04-21 20:41:15 +0800

    解决跨域问题

---
+++ FDP-management-business/FDP-gateway/src/main/java/com/zsinda/fdp/config/CorsConfig.java
@@ -0,0 +1,29 @@
+package com.zsinda.fdp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.util.pattern.PathPatternParser;
+
+/**
+ * @program: FDPlatform
+ * @description: CorsConfig
+ * @author: Sinda
+ * @create: 2020-04-21 20:40:39
+ */
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsWebFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedMethod("*");
+        // 设置你要允许的网站域名，如果全允许则设为 *
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+        source.registerCorsConfiguration("/**", config);
+        return new CorsWebFilter(source);
+    }
+}
