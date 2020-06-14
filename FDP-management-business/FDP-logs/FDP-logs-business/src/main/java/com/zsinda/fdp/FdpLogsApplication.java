684622178136aa0d61e0abce7378910036e71450
sindaZeng <1692488900@qq.com>
2020-06-14 19:55:19 +0800

    commit

--- FDP-management-business/FDP-logs/FDP-logs-business/src/main/java/com/zsinda/fdp/FdpLogsApplication.java
+++ FDP-management-business/FDP-logs/FDP-logs-business/src/main/java/com/zsinda/fdp/FdpLogsApplication.java
@@ -1,14 +1,14 @@
 package com.zsinda.fdp;
 
-import com.zsinda.fdp.annotation.EnableDynamicDataSource;
+import com.zsinda.fdp.annotation.EnableFdpResourceServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
-//@EnableFdpResourceServer
-@EnableDynamicDataSource
+@EnableFdpResourceServer
+//@EnableDynamicDataSource
 public class FdpLogsApplication {
 
     public static void main(String[] args) {
