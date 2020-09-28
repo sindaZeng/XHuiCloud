08eb4b3146c2e35abb43f4c932355f7417036cbd
sindaZeng <1692488900@qq.com>
2020-09-28 12:07:43 +0800

    更新脚本

--- doc/script/windows/host.bat
+++ doc/script/windows/host.bat
@@ -1,4 +1,8 @@
 @attrib -r "%windir%\system32\drivers\etc\hosts"
 
-@echo 120.0.0.1  xhuicloud_register >>"%windir%\system32\drivers\etc\hosts"
+@echo 120.0.0.1  xhuicloud-register >>"%windir%\system32\drivers\etc\hosts"
+
+@echo 120.0.0.1  xhuicloud-sentinel >>"%windir%\system32\drivers\etc\hosts"
+
+@echo 120.0.0.1  xhuicloud-mysql >>"%windir%\system32\drivers\etc\hosts"
 
