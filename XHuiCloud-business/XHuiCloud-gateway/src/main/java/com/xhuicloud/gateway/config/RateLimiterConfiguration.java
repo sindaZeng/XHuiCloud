e3d6b302e608ef3155f7e6578c71028de987e2d5
Sinda <1692488900@qq.com>
2021-12-25 09:48:47 +0800

    fix cors

--- XHuiCloud-business/XHuiCloud-gateway/src/main/java/com/xhuicloud/gateway/config/RateLimiterConfiguration.java
+++ XHuiCloud-business/XHuiCloud-gateway/src/main/java/com/xhuicloud/gateway/config/RateLimiterConfiguration.java
@@ -3,6 +3,9 @@ package com.xhuicloud.gateway.config;
 import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import reactor.core.publisher.Mono;
 
 @Configuration
@@ -11,6 +14,21 @@ public class RateLimiterConfiguration {
     @Bean(value = "remoteAddrKeyResolver")
     public KeyResolver remoteAddrKeyResolver() {
         return exchange -> Mono.just(exchange.getRequest().getRemoteAddress().getAddress().getHostAddress());
+    }
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.setMaxAge(600L);
+
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsWebFilter(source);
     }
 
 }
