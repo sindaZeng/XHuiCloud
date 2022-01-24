a4d7f8cd6e466b55a3b441c531ff8c89c8696cee
Sinda <1692488900@qq.com>
2022-01-24 14:09:37 +0800

    fix 自定义endpoint中自定义错误信息不生效

--- XHuiCloud-auth/src/main/java/com/xhuicloud/auth/controller/OAuthTokenEndpoint.java
+++ XHuiCloud-auth/src/main/java/com/xhuicloud/auth/controller/OAuthTokenEndpoint.java
@@ -25,21 +25,33 @@
 package com.xhuicloud.auth.controller;
 
 import com.xhuicloud.common.core.utils.Response;
-import lombok.AllArgsConstructor;
+import com.xhuicloud.common.security.component.XHuiWebResponseExceptionTranslator;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.common.exceptions.BadClientCredentialsException;
+import org.springframework.security.oauth2.common.exceptions.OAuth2Exception;
+import org.springframework.security.oauth2.provider.ClientRegistrationException;
 import org.springframework.security.oauth2.provider.endpoint.TokenEndpoint;
+import org.springframework.security.oauth2.provider.error.WebResponseExceptionTranslator;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.*;
 import java.security.Principal;
 import java.util.*;
 
+@Slf4j
 @RestController
 @RequestMapping("/oauth")
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class OAuthTokenEndpoint {
 
     private final TokenEndpoint tokenEndpoint;
+
+    /**
+     * 修改Oauth2定义的错误信息 为我们定义的错误信息
+     */
+    private WebResponseExceptionTranslator<OAuth2Exception> providerExceptionHandler = new XHuiWebResponseExceptionTranslator();
 
     @RequestMapping(value = "/token", method=RequestMethod.POST)
     public Response<OAuth2AccessToken> postAccessToken(Principal principal, @RequestParam
@@ -47,6 +59,38 @@ public class OAuthTokenEndpoint {
         ResponseEntity<OAuth2AccessToken> oAuth2AccessTokenResponseEntity = tokenEndpoint.postAccessToken(principal, parameters);
         OAuth2AccessToken body = oAuth2AccessTokenResponseEntity.getBody();
         return Response.success(body);
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<OAuth2Exception> handleHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException e) throws Exception {
+        if (log.isInfoEnabled()) {
+            log.info("Handling error: " + e.getClass().getSimpleName() + ", " + e.getMessage());
+        }
+        return providerExceptionHandler.translate(e);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<OAuth2Exception> handleException(Exception e) throws Exception {
+        if (log.isErrorEnabled()) {
+            log.error("Handling error: " + e.getClass().getSimpleName() + ", " + e.getMessage(), e);
+        }
+        return providerExceptionHandler.translate(e);
+    }
+
+    @ExceptionHandler(ClientRegistrationException.class)
+    public ResponseEntity<OAuth2Exception> handleClientRegistrationException(Exception e) throws Exception {
+        if (log.isWarnEnabled()) {
+            log.warn("Handling error: " + e.getClass().getSimpleName() + ", " + e.getMessage());
+        }
+        return providerExceptionHandler.translate(new BadClientCredentialsException());
+    }
+
+    @ExceptionHandler(OAuth2Exception.class)
+    public ResponseEntity<OAuth2Exception> handleException(OAuth2Exception e) throws Exception {
+        if (log.isWarnEnabled()) {
+            log.warn("Handling error: " + e.getClass().getSimpleName() + ", " + e.getMessage());
+        }
+        return providerExceptionHandler.translate(e);
     }
 
 }
