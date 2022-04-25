0654cdbc947ba30818e0ef232e68d3eaa5509411
xinda.zeng <1692488900@qq.com>
2022-04-25 14:58:56 +0800

    :fire: 删除setCreateId 代码。由AutoFieldMetaObjectHandler 统一执行

--- XHuiCloud-upms/XHuiCloud-upms-service/src/main/java/com/xhuicloud/upms/service/impl/SysParamServiceImpl.java
+++ XHuiCloud-upms/XHuiCloud-upms-service/src/main/java/com/xhuicloud/upms/service/impl/SysParamServiceImpl.java
@@ -29,11 +29,9 @@ import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.xhuicloud.common.core.constant.CacheConstants;
 import com.xhuicloud.common.core.exception.SysException;
-import com.xhuicloud.common.security.utils.SecurityHolder;
 import com.xhuicloud.upms.entity.SysParam;
 import com.xhuicloud.upms.mapper.SysParamMapper;
 import com.xhuicloud.upms.service.SysParamService;
-import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -61,7 +59,6 @@ public class SysParamServiceImpl extends ServiceImpl<SysParamMapper, SysParam> i
                 .eq(SysParam::getParamKey,sysParam.getParamKey())))){
             throw SysException.sysFail(SysException.PARAM_IS_EXIST_DATA_EXCEPTION);
         }
-        sysParam.setCreateId(SecurityHolder.getUserId());
         return save(sysParam);
     }
 }
